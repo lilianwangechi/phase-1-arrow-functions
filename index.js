24729f811800bf98d6da4091906f4b75f5efa2d6
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-10 05:53:00 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+// const divide = function(x ,y){
+//   return Math.floor(x/y);
+// }
+// function divide(x,y){
+//   return x/y;
+// }
+// let divide = function(x,y){
+//   return x/y
+// }
+let divide =function( x=2000,y =100){
+  return (x/y);
+};
+
+
+const square = (x) => x*x;
+
+const add = (x,y)=> x + y;
+
